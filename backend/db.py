2c783ce1cd706153489a7d27c7aa1f4debe6eb1c
aioria <aioria123.aza@gmail.com>
2026-07-01 15:21:45 -0600

    nube

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,11 @@
 import mysql.connector
+import os
 
 def conectar():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="examai"
+        host=os.environ.get("margaradb-aioria123.e.aivencloud.com"),
+        user=os.environ.get("avnadmin"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("defaultdb"),
+        port=os.environ.get("16443") # Usamos el puerto de Aiven
     )
