1fca9d1ac3881b753562b36f6176b1b2a60bfcbb
Carlos Daniel Perea Blas <cdblasp@gmail.com>
2026-07-22 00:48:29 -0600

    cambio 23

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect, url_for
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 from datetime import datetime, date, timedelta
 import mysql.connector
 import os
